3a14101c92ebf48be56c8ba464fbe7723960cc2c
KyleAnthonyHay <haykyle917@gmail.com>
2025-01-10 17:50:00 -0500

    Initial Commit

---
+++ GroupBox_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupBox_Test
+//
+//  Created by Kyle-Anthony Hay on 1/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupBox_Test/GroupBox_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBox_TestApp.swift
+//  GroupBox_Test
+//
+//  Created by Kyle-Anthony Hay on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GroupBox_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
